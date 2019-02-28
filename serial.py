ee9ce7f57cf89a9b7180c9051b287d32cfebbcd8
Manuela Carrasco <mcarras1@eafit.edu.co>
2019-02-28 04:21:42 -0500

    Serial with errors in sort

--- serial.py
+++ serial.py
@@ -7,42 +7,28 @@ from sys import maxsize
 field_size_limit(maxsize)
 
 def find_word(news, word):
-    count_tit = 0
-    count_des = 0
-    for title, title_word in enumerate(news[0].split(' ')):
-        if title_word == word:
-            count_tit += 1
-    for des, des_word in enumerate(news[1].split(' ')):
-        if des_word == word:
-            count_des += 1
+    count_tit = news[0].count(word)
+    count_des = news[1].count(word)
     return count_tit + count_des
 
 word = input("Ingrese la palabra a buscar: ")
 articles = ['../all-articles/articles1.csv', '../all-articles/articles2.csv', '../all-articles/articles3.csv']
-#articles=["prueba.csv"]
 
-biggest = []
 for article in articles:
     with open(article) as csv_file:
-        cont = 0
         csv_reader = read_csv(csv_file, usecols=[1,2,9])
         word = word.lower()
-        print_news = []
+        temp = []
         for row in csv_reader.values.tolist():
-            title = row[1]
-            news = row[2].lower()
-            if type(title) is str:
-                title = title.lower()
-                if title.find(word) > 0 or news.find(word) > 0:
-                    count = find_word((title, news), word)
-                    if cont < 10:
-                        print_news.append([count, row[0], row[1]])
-                    else:
-                        print_news.sort(reverse=True)
-                        print_news.append([count, row[1], row[2]])
-                        print_news.pop(len(print_news) - 1)
-                    cont += 1
+            title = str(row[1]).lower()
+            news = str(row[2]).lower()
+            count = find_word((title, news), word)
+            if count > 0:
+                temp.append([count, row[0], row[1]])
+    csv_file.close()
 
-for to_print in print_news:
+print_news = sorted(temp, key=lambda x: x[0])
+print_news.reverse()
+for to_print in print_news[:10]:
     print(to_print[0], to_print[1], to_print[2])
 
